5331125ecfdde83f8bced195f7c2804b882fe45f
Kristina Valenzuela <70911145+kristinav910@users.noreply.github.com>
2021-03-01 20:52:32 -0700

    Update dapp.js

--- frontend/dapp.js
+++ frontend/dapp.js
@@ -1,5 +1,5 @@
 // @TODO: Update this address to match your deployed MartianMarket contract!
-const contractAddress = "0x7a377fAd8c7dB341e662c93A79d0B0319DD3DaE8";
+const contractAddress = "0x92A45407C98E344Bbb4387380785AAFBf724e105";
 
 const dApp = {
   ethEnabled: function() {
